c13ab72a3342abe5a8e4b84c56cc4f190b720f05
Jan-Philipp Benecke <jpbede@users.noreply.github.com>
2017-08-31 09:25:23 +0200

    disabled ssl verification
    disabled ssl verification because old openssl version in docker container

--- src/Citrix/ServiceAbstract.php
+++ src/Citrix/ServiceAbstract.php
@@ -80,6 +80,8 @@ abstract class ServiceAbstract
       curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     }
     
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return the output in string format
     $output = curl_exec($ch); // execute
